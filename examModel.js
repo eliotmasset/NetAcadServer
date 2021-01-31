305b651f84b480b7dd55b85ff985d321240da505
Aimuhire <marsanline@gmail.com>
2021-01-31 04:25:07 +0200

    Add creation timestamps and make pageUrl unique

--- examModel.js
+++ examModel.js
@@ -1,42 +1,41 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/ccnaSelectDb', { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/ccnaSelectDb", { useNewUrlParser: true });
 
 const Schema = mongoose.Schema;
 
 const examSchema = new Schema({
-
-    name: String,
-    version: String,
-    pageUrl:  { type: String    },//, unique: true }, 
-    questions: [
-        {
-            title: String,
-            questionImage: String,
-            identifier: { type: String  }, 
-            solution: {
-                choices: [
-                    {
-                        name: String,
-                        isAnswer: Boolean,
-                    }
-                ],
-                choiceImages: [
-                    {
-                        name: String,
-                        isAnswer: Boolean,
-                    }
-                ],
-                solutionImage: String,
-                htmlSolution: String,
-                explanation: String,
-            },
-
-        }
-    ]
+  name: String,
+  version: String,
+  pageUrl: { type: String, unique: true },
+  creationTimestamp: { type: Date, default: Date.now() },
+  questions: [
+    {
+      title: String,
+      questionImage: String,
+      identifier: { type: String },
+      creationTimestamp: { type: Date, default: Date.now() },
+      solution: {
+        choices: [
+          {
+            name: String,
+            isAnswer: Boolean,
+          },
+        ],
+        choiceImages: [
+          {
+            name: String,
+            isAnswer: Boolean,
+          },
+        ],
+        solutionImage: String,
+        htmlSolution: String,
+        explanation: String,
+      },
+    },
+  ],
 });
 
-const qModel = mongoose.model('exam', examSchema)
+const qModel = mongoose.model("exam", examSchema);
 
-module.exports = qModel
+module.exports = qModel;
